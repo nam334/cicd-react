a136e957d8dc4ca4b5f78838a21d6e8dcd2bf8da
nam334 <namratadas334@gmail.com>
2025-04-03 13:07:17 +0530

    Test commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
-  { ignores: ["dist", ".eslintrc.cjs", "vite.config.js"] },
+  { ignores: ['dist', '.eslintrc.cjs', 'vite.config.js'] },
 
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     plugins: {
       react: react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       prettier: prettier,
     },
     rules: {
@@ -30,17 +30,17 @@ export default [
       ...reactHooks.configs.recommended.rules,
       ...prettier.configs.recommended.rules,
 
-      "react/react-in-jsx-scope": "off",
-      "react-refresh/only-export-components": [
-        "warn",
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "prettier/prettier": "error",
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      'prettier/prettier': 'error',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
